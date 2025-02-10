92bcb02b797160df6c9b4b880a6d8a945054f63c
Dustin Tran <dustintr@udel.edu>
2025-02-10 10:41:01 -0500

    Retry message

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("หากคุณแปลประโยคนี้ ยินดีด้วย ฉันเสียเวลาไป 25 วินาทีในชีวิตของคุณ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
